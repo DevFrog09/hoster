61e58253f612b04770e7a965b0dbe2419c62e696
DevFrog09 <114627020+DevFrog09@users.noreply.github.com>
2022-10-29 15:55:53 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const yesBtn = document.querySelector('#yes');
+
+yesBtn.addEventListener('click',function () {
+    alert('mandame un mensaje que dija queso jajaja')
+});
+
+const noBtn = document.querySelector('#no');
+
+noBtn.addEventListener('click', function () {
+    alert(":( jajaja")
+})
+
